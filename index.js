52678cfc0ee1b49be9be79c71902a3ef6a1a7d3f
forhadhossain <forhadhossainopc@gmail.com>
2023-11-02 13:25:11 +0600

    evrything updated success

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3worizk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,17 +21,44 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    ProductsCollection = client.db('emajhonDB').collection('products')
 
-    const productCollection = client.db('emaJohnDB').collection('products');
 
-    app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
+    // main oparations
+    app.get('/products', async (req,res)=> {
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+      console.log(req.query)
+      
+      const find = ProductsCollection.find().skip(page*size).limit(size)
+      const result = await find.toArray();
+       res.send(result)
     })
+
+
+    app.post('/productByIds', async (req,res)=>{
+      const ids = req.body;
+      const idWithObjectId = ids.map(id => new ObjectId(id))
+      const query = { _id: { $in: idWithObjectId}};
+      const result = await ProductsCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/productscount', async (req,res)=> {
+      const count = await ProductsCollection.estimatedDocumentCount();
+       res.send({count})
+    })
+
+
+
+   
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -44,6 +69,8 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) =>{
